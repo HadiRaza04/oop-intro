5b3134cac1718729bca382104a58fe9a9a7488ea
Hadi Raza <hadiraza.edu@gmail.com>
2023-01-31 00:06:06 +0500

    1-method added

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// class grattings{
-//     hello(){
-//         console.log("Hello Everyone!");
-//     }
-//     salam(){
-//         console.log("Salam Everyone!");
-//     }
-// }
-// let a = new grattings();
-// a.hello();
-// a.salam();
+class grattings{
+    hello(){
+        console.log("Hello Everyone!");
+    }
+    salam(){
+        console.log("Salam Everyone!");
+    }
+}
+let a = new grattings();
+a.hello();
+a.salam();
+
 
 //---------------------------------- METHOD TYPES -----------------------------//
-// There are Three types of class methods in JavaScript.
+//There are Three types of class methods in JavaScript.
 
 // The first one is Constructor Method.
 // The second one is Instance/Prototype Method.
 // The third one is Static Method.
-
-
 class student{
     constructor(name, age, email){
         this.username = name;
@@ -28,18 +27,13 @@ class student{
     hello(){
         document.write(`Hi ${this.username}, your age is ${this.userage}.`);
     }
+    static staticMethod(){
+        console.log("Static Method called");
+    }
 }
 let b = new student("Raza",20);
 let c = new student("Hadi",21);
 b.hello();
 c.hello();
-
-
-
-
-
-
-
-
-
-
+// b.ststicMethod();          ------ This will not work
+student.staticMethod();       // this will work properly
